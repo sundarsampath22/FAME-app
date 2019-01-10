7263b9eba8542e6f790ac9a4426a6aa5967cf551
Collin Griffin <collinjgriffin@gmail.com>
2019-01-10 17:16:03 -0500

    Adds user authentication using email and password. Includes creating account, logging in, and signing out.

--- Application/src/main/java/com/example/android/notificationchannels/LoginActivity.java
+++ Application/src/main/java/com/example/android/notificationchannels/LoginActivity.java
@@ -1,13 +1,118 @@
 package com.example.android.notificationchannels;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends Activity {
 
-    /*@Override
+    private EditText Email;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private TextView CreateAccount;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressBar loadingBar;
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }*/
+
+        Email = findViewById(R.id.et_email);
+        Password = findViewById(R.id.et_password);
+        Login = findViewById(R.id.btn_login);
+        CreateAccount = findViewById(R.id.tv_create_account);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loadingBar = findViewById(R.id.loginProgress);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Email.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        CreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToCreateAccount();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null)
+            sendToMain();
+    }
+
+    private void validate (String userEmail, String userPassword)
+    {
+        if(TextUtils.isEmpty(userEmail))
+        {
+            Toast.makeText(this, "Please include your email", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(userPassword))
+        {
+            Toast.makeText(this, "Please include your password", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setVisibility(ProgressBar.VISIBLE);
+
+            mAuth.signInWithEmailAndPassword(userEmail, userPassword)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                                sendToMain();
+                                loadingBar.setVisibility(ProgressBar.INVISIBLE);
+                            }
+                            else
+                            {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, "Error occured: " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.setVisibility(ProgressBar.INVISIBLE);
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void sendToMain() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+    private void sendToCreateAccount() {
+        Intent createAccountIntent = new Intent(LoginActivity.this, NewAccountActivity.class);
+        startActivity(createAccountIntent);
+    }
 }
--- Application/src/main/java/com/example/android/notificationchannels/MainActivity.java
+++ Application/src/main/java/com/example/android/notificationchannels/MainActivity.java
@@ -17,164 +17,146 @@
 package com.example.android.notificationchannels;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-/**
- * Display main screen for sample. Displays controls for sending test notifications.
- */
-public class MainActivity extends Activity {
-    private static final String TAG = MainActivity.class.getSimpleName();
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    private static final int NOTI_PRIMARY1 = 1100;
-    private static final int NOTI_PRIMARY2 = 1101;
-    private static final int NOTI_SECONDARY1 = 1200;
-    private static final int NOTI_SECONDARY2 = 1201;
+public class MainActivity extends AppCompatActivity {
 
-    /*
-     * A view model for interacting with the UI elements.
-     */
-    private MainUi ui;
+    private DrawerLayout mDrawerLayout;
+    private FirebaseAuth mAuth;
+    private DatabaseReference userRef;
 
-    /*
-     * A
-     */
-    private NotificationHelper noti;
+
+    String currentUserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        noti = new NotificationHelper(this);
-        ui = new MainUi(findViewById(R.id.activity_main));
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        userRef = FirebaseDatabase.getInstance().getReference().child("users");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // close drawer when item is tapped
+                        switch (menuItem.getItemId())
+                        {
+                            case R.id.nav_new_post:
+                                mDrawerLayout.closeDrawers();
+                                sendToNewPost();
+                                break;
+                            case R.id.nav_feed:
+                                mDrawerLayout.closeDrawers();
+                                break;
+                            case R.id.nav_profile:
+                                mDrawerLayout.closeDrawers();
+                                break;
+                            case R.id.nav_logout:
+                                mDrawerLayout.closeDrawers();
+                                mAuth.signOut();
+                                sendToLogin();
+                                break;
+                        }
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
     }
 
-    /**
-     * Send activity notifications.
-     *
-     * @param id The ID of the notification to create
-     * @param title The title of the notification
-     */
-    public void sendNotification(int id, String title) {
-        Notification.Builder nb = null;
-        switch (id) {
-            case NOTI_PRIMARY1:
-                nb = noti.getNotification1(title, getString(R.string.primary1_body));
-                break;
 
-            case NOTI_PRIMARY2:
-                nb = noti.getNotification1(title, getString(R.string.primary2_body));
-                break;
-
-            case NOTI_SECONDARY1:
-                nb = noti.getNotification2(title, getString(R.string.secondary1_body));
-                break;
-
-            case NOTI_SECONDARY2:
-                nb = noti.getNotification2(title, getString(R.string.secondary2_body));
-                break;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
-        if (nb != null) {
-            noti.notify(id, nb);
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null)
+        {
+            sendToLogin();
+        }
+        else
+        {
+            checkUserExistence();
         }
     }
 
-    /**
-     * Send Intent to load system Notification Settings for this app.
-     */
-    public void goToNotificationSettings() {
-        Intent i = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
-        i.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
-        startActivity(i);
-    }
+    private void checkUserExistence()
+    {
+        final String USER_ID = mAuth.getCurrentUser().getUid();
 
-    /**
-     * Send intent to load system Notification Settings UI for a particular channel.
-     *
-     * @param channel Name of channel to configure
-     */
-    public void goToNotificationSettings(String channel) {
-        Intent i = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
-        i.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
-        i.putExtra(Settings.EXTRA_CHANNEL_ID, channel);
-        startActivity(i);
-    }
-
-    /**
-     * View model for interacting with Activity UI elements. (Keeps core logic for sample
-     * seperate.)
-     */
-    class MainUi implements View.OnClickListener {
-        final TextView titlePrimary;
-        final TextView titleSecondary;
-
-        private MainUi(View root) {
-            titlePrimary = (TextView) root.findViewById(R.id.main_primary_title);
-            ((Button) root.findViewById(R.id.main_primary_send1)).setOnClickListener(this);
-            ((Button) root.findViewById(R.id.main_primary_send2)).setOnClickListener(this);
-            ((ImageButton) root.findViewById(R.id.main_primary_config)).setOnClickListener(this);
-
-            titleSecondary = (TextView) root.findViewById(R.id.main_secondary_title);
-            ((Button) root.findViewById(R.id.main_secondary_send1)).setOnClickListener(this);
-            ((Button) root.findViewById(R.id.main_secondary_send2)).setOnClickListener(this);
-            ((ImageButton) root.findViewById(R.id.main_secondary_config)).setOnClickListener(this);
-
-            ((Button) root.findViewById(R.id.btnA)).setOnClickListener(this);
-        }
-
-        private String getTitlePrimaryText() {
-            if (titlePrimary != null) {
-                return titlePrimary.getText().toString();
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if(!dataSnapshot.hasChild(USER_ID))
+                    sendToSetup();
             }
-            return "";
-        }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
 
-        private String getTitleSecondaryText() {
-            if (titlePrimary != null) {
-                return titleSecondary.getText().toString();
             }
-            return "";
-        }
+        });
+    }
 
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.main_primary_send1:
-                    sendNotification(NOTI_PRIMARY1, getTitlePrimaryText());
-                    break;
-                case R.id.main_primary_send2:
-                    sendNotification(NOTI_PRIMARY2, getTitlePrimaryText());
-                    break;
-                case R.id.main_primary_config:
-                    goToNotificationSettings(NotificationHelper.PRIMARY_CHANNEL);
-                    break;
+    private void sendToSetup() {
+        Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
+        setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(setupIntent);
+        finish();
+    }
 
-                case R.id.main_secondary_send1:
-                    sendNotification(NOTI_SECONDARY1, getTitleSecondaryText());
-                    break;
-                case R.id.main_secondary_send2:
-                    sendNotification(NOTI_SECONDARY2, getTitleSecondaryText());
-                    break;
-                case R.id.main_secondary_config:
-                    goToNotificationSettings(NotificationHelper.SECONDARY_CHANNEL);
-                    break;
-                case R.id.btnA:
-                    goToNotificationSettings();
-                    break;
-                default:
-                    Log.e(TAG, "Unknown click event.");
-                    break;
-            }
-        }
+    private void sendToLogin() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
+    }
+
+    private void sendToNewPost() {
+        Intent newPostIntent = new Intent(MainActivity.this, PostActivity.class);
+        newPostIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(newPostIntent);
     }
 }
